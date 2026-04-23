d087cce57c86f6a9665d1181627e7fe1e5321093
Mohan Gowda R <mohanrmoni54@gmail.com>
2026-04-23 08:19:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,33 @@ def greet():
     return jsonify(message="Hello, User!")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
+
+
+
+Dockerfile
+# Use official Python image
+FROM python:3.9-slim
+# Set working directory
+WORKDIR /app
+# Copy files
+COPY . /app
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+# Expose port
+EXPOSE 5000
+# Run application
+CMD ["python", "app.py"]
+
+requirements.txt
+flask
+
+
+
+
+
+docker build -t flask-app .
+docker run -p 5000:5000 flask-app
+
+open in browser
+http://localhost:5000/greet
